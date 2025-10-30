07a8f1ef1a8f90f06fff71b6d202ac84d795094a
Tobias <toby.janca@seznam.cz>
2025-10-30 10:15:21 +0100

    feat: support bursa (in_exchange)

--- webkredit/models.py
+++ webkredit/models.py
@@ -6,9 +6,10 @@ class Meal:
         self.available = meal_data.get("countAvailable", None)
         self.ordered_total = meal_data.get("countOrdered", None)
         self.alergens = meal_data.get("note", None)
+        self.in_exchange = meal_data.get("inExchange", None)
 
         if self.name is None or self.number is None or self.ordered_total is None:
             raise ValueError("Jídlo obsahuje neplatné informace, prosím, kontaktuje vývojáře.")
 
     def __str__(self):
-        return f"Jídlo {self.number}: {self.name} | Počet dostupných jídel: {self.available if self.available is not None else 'neomezeno'} | Objednáno porcí celkem: {self.ordered_total} | Alergeny: {self.alergens}"
+        return f"Jídlo {self.number}: {self.name} | Počet dostupných jídel: {self.available if self.available is not None else 'neomezeno'} | Objednáno porcí celkem: {self.ordered_total} | Alergeny: {self.alergens} | Je na burze: {'ano' if self.in_exchange else 'ne'}"
